0f62931f6be1e1464a46b4d3bc198b1e426c1b07
Bastian <bastianlc@live.cl>
2021-03-12 18:32:31 -0300

    add ticket app, model and admin

--- core/ticket/admin.py
+++ core/ticket/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Ticket
 
 # Register your models here.
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ["title", "state", "created_at"]
+
+
+admin.site.register(Ticket, TicketAdmin)
--- core/ticket/models.py
+++ core/ticket/models.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Ticket(models.Model):
+    STATE_CHOICES = (
+        ('1', 'Abierto'),
+        ('2', 'Pendiente'),
+        ('3', 'En Proceso'),
+        ('4', 'Resuelto'),
+        ('5', 'Cerrado'),
+    )
+
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    state = models.CharField(max_length=200, choices = STATE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Ticket"
+        verbose_name_plural = "Tickets"
