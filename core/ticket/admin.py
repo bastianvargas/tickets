d9a56a45e3ee078d74e5df164ec7c02cd1dbbfb3
Bastian <bastianlc@live.cl>
2021-03-15 02:31:45 -0300

    custom ticket admin

--- core/ticket/admin.py
+++ core/ticket/admin.py
@@ -2,12 +2,24 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Ticket
+from .models import Ticket, State
 
 # Register your models here.
 
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ["title", "state", "created_at"]
+    fields_ticket = ["id", "title", "state", "created_at"]
+    list_display = fields_ticket
+    search_fields = fields_ticket
+    list_filter = fields_ticket
+    readonly_fields = ('created_at',)
+    ordering = ['created_at']
 
+class StateAdmin(admin.ModelAdmin):
+
+    list_display = ['name']
+    search_fields = ['name']
+    list_filter = ['name']
+    ordering = ['name']
 
 admin.site.register(Ticket, TicketAdmin)
+admin.site.register(State, StateAdmin)
